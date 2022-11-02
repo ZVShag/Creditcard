55421ec86e34f508c5fa8dc170257a2ce078feee
Alexey Alexey <alextutor72@gmail.com>
2022-11-02 17:53:52 +0500

    add class

--- Try/Program.cs
+++ Try/Program.cs
@@ -1,1 +1,13 @@
 ﻿
+using Try;
+
+Console.WriteLine("Введите номер карты:");
+int number=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите ФИО держателя карты:");
+string fio = Console.ReadLine();
+Console.WriteLine("Введите дату действия карты:");
+string date = Console.ReadLine();
+Console.WriteLine("Введите cvc карты:");
+int cvc = int.Parse(Console.ReadLine());
+
+Creditcard card = new Creditcard(number,fio,date,cvc);
